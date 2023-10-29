e1a082a4aa7315b20bd18882b921baf30a6929af
unknown <contribution0526@gmail.com>
2023-10-29 21:57:19 +0900

    Improve the README file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
-module ApplicationHelper
-end
+ module ApplicationHelper
+
+        # ページごとの完全なタイトルを返します。
+        def full_title(page_title = '')
+          base_title = "Ruby on Rails Tutorial Sample App"
+          if page_title.empty?
+            base_title                  #home pageのタイトルは空だから、base titleが表示される
+          else
+            page_title + " | " + base_title #page_titleが空じゃなかったら例 Help|Ruby on Rails ....
+          end
+        end
+ end
+
